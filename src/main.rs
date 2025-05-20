3cd1975c0059dc565498e4284601a4a3b52ef6c6
SynthHorizon <blizzard.horizon@gmail.com>
2025-05-20 16:16:47 +0300

    made it globally accessible and changed frontend location

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ use std::{
 use baller::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+//    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
@@ -29,12 +30,8 @@ fn handle_connection(mut stream: TcpStream) {
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
-       "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-       "GET /sleep HTTP/1.1" => {
-           thread::sleep(Duration::from_secs(5));
-           ("HTTP/1.1 200 OK", "hello.html")
-       }
-       _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+       "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "frontend/hello.html"),
+       _ => ("HTTP/1.1 404 NOT FOUND", "frontend/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -44,5 +41,4 @@ fn handle_connection(mut stream: TcpStream) {
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
 }
